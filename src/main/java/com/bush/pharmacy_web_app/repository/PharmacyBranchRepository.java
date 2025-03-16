84236c67a51469a258e256f95e918e11c0790af2
Dmitry Bush <bushuev0458@gmail.com>
2025-03-16 18:33:38 +0300

    Completed order repository test

---
+++ src/main/java/com/bush/pharmacy_web_app/repository/PharmacyBranchRepository.java
@@ -0,0 +1,7 @@
+package com.bush.pharmacy_web_app.repository;
+
+import com.bush.pharmacy_web_app.repository.entity.PharmacyBranch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PharmacyBranchRepository extends JpaRepository<PharmacyBranch, Integer> {
+}
