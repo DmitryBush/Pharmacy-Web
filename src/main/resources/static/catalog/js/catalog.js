79de9f1d1187d6acdc1a473e58288d76b236080f
Dmitry Bush <bushuev0458@gmail.com>
2025-03-19 18:22:23 +0300

    Improved catalog filtration
    - Now when you click a filter, the filter is automatically applied
    - Filter reset button is fixed

---
+++ src/main/resources/static/catalog/js/catalog.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+        // Парсинг параметров из URL
+        const urlParams = new URLSearchParams(window.location.search);
+
+        // Установка значений из URL
+        urlParams.getAll("type").forEach(value => {
+            document.querySelectorAll(`input[name="type"][value="${value}"]`).forEach(checkbox => {
+                checkbox.checked = true;
+            });
+        });
+
+        urlParams.getAll("manufacturer").forEach(value => {
+            document.querySelectorAll(`input[name="manufacturer"][value="${value}"]`).forEach(checkbox => {
+                checkbox.checked = true;
+            });
+        });
+
+        const recipeValue = urlParams.get("recipe");
+        if (recipeValue) {
+            document.querySelector(`input[name="recipe"][value="${recipeValue}"]`).checked = true;
+        }
+
+        document.getElementById("minPrice").value = urlParams.get("minPrice") || "";
+        document.getElementById("maxPrice").value = urlParams.get("maxPrice") || "";
+    });
+
+document.querySelector("form").addEventListener("change", function() {
+        const formData = new FormData(this);
+        const params = new URLSearchParams(formData).toString();
+        window.history.replaceState(null, null, "?" + params);
+        window.location.search = params;
+    });
+
+document.getElementById("resetFilters").addEventListener("click", function() {
+    document.querySelector("form").reset();
+
+    window.history.replaceState(null, null, window.location.pathname);
+
+    window.location.search = "";
+});
