496c1a89ee01cf10fc5aff06b473eb9c2c600d46
Dmitry Bush <bushuev0458@gmail.com>
2025-03-21 18:23:33 +0300

    Fixed customer order controller

--- src/main/java/com/bush/pharmacy_web_app/controllers/CustomerOrdersController.java
+++ src/main/java/com/bush/pharmacy_web_app/controllers/CustomerOrdersController.java
@@ -1,33 +1,28 @@
 package com.bush.pharmacy_web_app.controllers;
 
-import com.bush.pharmacy_web_app.repository.dto.orders.OrderReadDto;
 import com.bush.pharmacy_web_app.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 
 import java.util.Objects;
 
 
 @RequestMapping("api/v1/orders")
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class CustomerOrdersController {
     private final CustomerService customerService;
-    private final TemplateEngine templateEngine;
 
-    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
-    public String findAllOrders() {
-        PageImpl<OrderReadDto> page = new PageImpl<>(
+    @GetMapping
+    public String findAllOrders(Model model) {
+        var page = new PageImpl<>(
                 Objects.requireNonNull(customerService.findById("+79221234567").orElse(null)).orders());
 
-        Context context = new Context();
-        context.setVariable("ordersPage", page);
-        return templateEngine.process("order/order", context);
+        model.addAttribute("ordersPage", page);
+        return "order/order";
     }
 }
