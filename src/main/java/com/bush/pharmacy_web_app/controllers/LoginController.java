ff10f7bfcf17dfce9b96cca68a34199a60b16b6a
Dmitry Bush <bushuev0458@gmail.com>
2025-03-22 09:28:15 +0300

    Update LoginController.java

--- src/main/java/com/bush/pharmacy_web_app/controllers/LoginController.java
+++ src/main/java/com/bush/pharmacy_web_app/controllers/LoginController.java
@@ -8,17 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
-@RestController
-@RequestMapping("/api/v1/login")
+@Controller
+@RequestMapping("/login")
 @RequiredArgsConstructor
 public class LoginController {
-    private final TemplateEngine templateEngine;
 
     @GetMapping
     public String getSite() {
-        Context context = new Context();
-
-        return templateEngine.process("user/login/login", context);
+        return "user/login/login";
     }
 
 }
