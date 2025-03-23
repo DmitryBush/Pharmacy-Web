c14b5d4027c653c770afba1e7aeef5972d611f62
Dmitry Bush <bushuev0458@gmail.com>
2025-03-23 13:17:32 +0300

    Added product card page

--- src/main/java/com/bush/pharmacy_web_app/controllers/CatalogController.java
+++ src/main/java/com/bush/pharmacy_web_app/controllers/CatalogController.java
@@ -1,6 +1,5 @@
 package com.bush.pharmacy_web_app.controllers;
 
-import com.bush.pharmacy_web_app.repository.dto.PageResponse;
 import com.bush.pharmacy_web_app.repository.filter.MedicineFilter;
 import com.bush.pharmacy_web_app.service.MedicineService;
 import lombok.RequiredArgsConstructor;
@@ -10,18 +9,19 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/v1/catalog")
+@RequestMapping("/catalog")
 @RequiredArgsConstructor
 public class CatalogController {
     private final MedicineService service;
 
     @GetMapping
-    public String findAllMedicines(Model model,
-                                   MedicineFilter filter,
-                                   @PageableDefault(size = 15, sort = "price", direction = Sort.Direction.ASC)
+    public String findAllProducts(Model model,
+                                  MedicineFilter filter,
+                                  @PageableDefault(size = 15, sort = "price", direction = Sort.Direction.ASC)
                                        Pageable pageable) {
         var page = service.findAll(filter, pageable);
 
@@ -29,5 +29,14 @@ public class CatalogController {
         model.addAttribute("manufacturers", service.findAllManufacturers());
         model.addAttribute("medicines", page);
         return "catalog/catalog";
+    }
+
+    @GetMapping("/{id}")
+    public String getProduct(Model model,
+                             @PathVariable Integer id) {
+        var product = service.findById(id).orElseThrow();
+
+        model.addAttribute("product", product);
+        return "catalog/product";
     }
 }
