73bd1aab3f16b26f8734ed2d934accaad4e9ce86
Dmitry Bush <bushuev0458@gmail.com>
2025-05-14 00:19:13 +0300

    Added back link to order management page

--- src/main/java/com/bush/pharmacy_web_app/controllers/admin/AdminOrderController.java
+++ src/main/java/com/bush/pharmacy_web_app/controllers/admin/AdminOrderController.java
@@ -4,22 +4,15 @@ import com.bush.pharmacy_web_app.repository.entity.order.OrderStatus;
 import com.bush.pharmacy_web_app.service.OrderService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Arrays;
 
 @Controller
 @RequestMapping("/admin/orders")
@@ -43,7 +36,7 @@ public class AdminOrderController {
     public String getOrder(@PathVariable Long id, Model model, HttpServletRequest httpRequest) {
         var order = orderService.findOrderById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        
+
         model.addAttribute("order", order);
         model.addAttribute("OrderStatus", OrderStatus.class);
         model.addAttribute("currentUri", httpRequest.getRequestURI());
