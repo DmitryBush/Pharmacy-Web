ea78b2ab50df845eacaee2aafecddb9d4f667fa8
Dmitry Bush <bushuev0458@gmail.com>
2025-07-22 21:45:05 +0300

    Added flag to check main product category

---
+++ src/main/java/com/bush/pharmacy_web_app/repository/entity/medicine/ProductCategories.java
@@ -0,0 +1,19 @@
+package com.bush.pharmacy_web_app.repository.entity.medicine;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product_categories")
+public class ProductCategories {
+    @EmbeddedId
+    private ProductCategoriesId id;
+
+    @Column(name = "is_main", nullable = false)
+    private Boolean isMain;
+}
---
+++ src/main/java/com/bush/pharmacy_web_app/repository/entity/medicine/ProductCategoriesId.java
@@ -0,0 +1,20 @@
+package com.bush.pharmacy_web_app.repository.entity.medicine;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+@IdClass(ProductCategoriesId.class)
+public class ProductCategoriesId {
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Medicine medicine;
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private MedicineType type;
+}
