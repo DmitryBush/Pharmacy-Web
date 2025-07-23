98cd4da20a6e0afb89266818fd17ca606cf2e327
Dmitry Bush <bushuev0458@gmail.com>
2025-07-23 15:13:49 +0300

    Implemented category search and path methods
    - Implemented native SQL method to search for Medicines in a specific category
    - implemented native SQL method to get the full path of a category

--- src/main/java/com/bush/pharmacy_web_app/repository/MedicineRepository.java
+++ src/main/java/com/bush/pharmacy_web_app/repository/MedicineRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -21,4 +22,23 @@ public interface MedicineRepository extends JpaRepository<Medicine, Long>, Filte
 
     @Query("select distinct m.manufacturer from Medicine m ")
     List<Manufacturer> findDistinctMedicineManufacturer();
+
+    @Query(value = """
+            WITH RECURSIVE subcategories AS (
+                        SELECT type_id
+                        FROM medicine_types\s
+                        WHERE type_id = :typeId
+                       \s
+                        UNION ALL
+                       \s
+                        SELECT c.type_id
+                        FROM medicine_types c
+                        INNER JOIN subcategories s ON c.parent_id = s.type_id
+                    )
+            SELECT DISTINCT p.*\s
+            FROM medicine p
+            JOIN product_categories pc ON p.medicine_id = pc.product_id
+            WHERE pc.category_id IN (SELECT type_id FROM subcategories)
+            """, nativeQuery = true)
+    List<Medicine> findAllMedicineByTypeId(@PathVariable Integer typeId);
 }
