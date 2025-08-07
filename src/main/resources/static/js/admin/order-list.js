eb8bcbfbf2d9e1c4088333a0a3b65d70805fa1f7
Dmitry Bush <bushuev0458@gmail.com>
2025-08-07 23:56:25 +0300

    Fixed cross-site vulnerability
    - The issue with extracting text from DOM resolved by proper validation input data
    - Fixed issue with resolving warehouse duplicates

--- src/main/resources/static/js/admin/order-list.js
+++ src/main/resources/static/js/admin/order-list.js
@@ -1,8 +1,23 @@
+import Notification from "../notification/notification.js";
 document.addEventListener('DOMContentLoaded', () => {
+    const notification = new Notification('/api/v1/icons/admin/orders.png');
     const branchId = document.getElementById('branch-id').dataset.id;
     document.querySelectorAll('.order-more-det').forEach(el => {
         el.addEventListener('click', (e) => {
-            window.location.replace(`/admin/orders/branch/${branchId}/order/${e.target.getAttribute('data-id')}`);
+            try {
+                const orderId = e.target.dataset.id;
+                if (!/^\d+$/.test(orderId))
+                    throw new Error('Произошла критическая ошибка при подготовке ресурсов. ' +
+                        'Обратитесь к вашему администратору');
+                if (!/^\d+$/.test(branchId))
+                    throw new Error('Произошла критическая ошибка при подготовке ресурсов. ' +
+                        'Обратитесь к вашему администратору');
+                window.location.replace(`/admin/orders/branch/${encodeURIComponent(branchId)}` +
+                    `/order/${encodeURIComponent(orderId)}`);
+            } catch (e) {
+                console.error(e);
+                notification.showNotification('Управление заказами', e.message);
+            }
         });
     });
 });
--- src/main/resources/static/js/admin/warehouse.js
+++ src/main/resources/static/js/admin/warehouse.js
@@ -1,7 +1,19 @@
+import Notification from "../notification/notification.js";
+
 document.addEventListener("DOMContentLoaded", () => {
+    const notification = new Notification('/api/v1/icons/admin/box-fill.png');
     document.querySelectorAll('.branch-more-det').forEach(el => {
         el.addEventListener('click', (e) => {
-            window.location.replace(`/admin/warehouse/${e.target.getAttribute('data-id')}`);
+            try {
+                const branchId = e.target.dataset.id;
+                if (!/^\d+$/.test(branchId))
+                    throw new Error('Произошла критическая ошибка при подготовке ресурсов. ' +
+                        'Обратитесь к вашему администратору');
+                window.location.replace(`/admin/warehouse/${encodeURIComponent(branchId)}`);
+            } catch (e) {
+                console.error(e);
+                notification.showNotification('Управление заказами', e.message);
+            }
         })
     })
 });
