d5a9663e095a71084e4c622f145bbf3d7f8aec82
Dmitry Bush <bushuev0458@gmail.com>
2025-10-10 15:37:51 +0300

    feat(newsImages):implemented news image endpoint

--- src/main/java/com/bush/pharmacy_web_app/controllers/rest/news/NewsImageRestController.java
+++ src/main/java/com/bush/pharmacy_web_app/controllers/rest/news/NewsImageRestController.java
@@ -1,10 +1,22 @@
 package com.bush.pharmacy_web_app.controllers.rest.news;
 
+import com.bush.pharmacy_web_app.service.news.NewsImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/news-images")
+@RequiredArgsConstructor
 public class NewsImageRestController {
+    private final NewsImageService newsImageService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Resource> getImageById(@PathVariable Long id) {
+        return ResponseEntity.ok(newsImageService.findImageById(id));
+    }
 }
--- src/main/java/com/bush/pharmacy_web_app/service/news/NewsImageService.java
+++ src/main/java/com/bush/pharmacy_web_app/service/news/NewsImageService.java
@@ -1,5 +1,25 @@
 package com.bush.pharmacy_web_app.service.news;
 
-public class NewsImageService {
+import com.bush.pharmacy_web_app.model.entity.news.NewsImage;
+import com.bush.pharmacy_web_app.repository.news.NewsImageRepository;
+import com.bush.pharmacy_web_app.service.filesystem.FileSystemStorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class NewsImageService {
+    private final NewsImageRepository imageRepository;
+
+    private final FileSystemStorageService fileSystemStorageService;
+
+    public Resource findImageById(Long imageId) {
+        return fileSystemStorageService.loadAsResource("news/" + imageId,
+                imageRepository.findById(imageId)
+                        .map(NewsImage::getImageWorkPath)
+                        .orElseThrow(IllegalArgumentException::new));
+    }
 }
