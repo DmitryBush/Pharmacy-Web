c7c172fe2d67a1c93b5f56fb1dc2aa2f90e0b8a9
Dmitry Bush <bushuev0458@gmail.com>
2025-10-10 17:49:15 +0300

    feat(newsManagement):implemented management page

---
+++ src/main/java/com/bush/pharmacy_web_app/controllers/http/admin/NewsManagementController.java
@@ -0,0 +1,31 @@
+package com.bush.pharmacy_web_app.controllers.http.admin;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin/news")
+public class NewsManagementController {
+
+    @GetMapping
+    public String getNewsManagementPage(Model model,
+                                        HttpServletRequest request,
+                                        @AuthenticationPrincipal UserDetails userDetails) {
+        var authorities = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        model.addAttribute("authorities", authorities);
+        model.addAttribute("currentUri", request.getRequestURI());
+        return "/admin/news-management";
+    }
+}
