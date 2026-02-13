f5105778a87f532e60e35bb48a8da237007c244d
Dmitry Bush <bushuev0458@gmail.com>
2026-02-13 15:28:38 +0300

    feat(debezium):Implemented debezium init service

---
+++ Search-Service/src/main/java/com/bush/search/SearchServiceApplication.java
@@ -0,0 +1,11 @@
+package com.bush.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchServiceApplication {
+    static void main(String[] args) {
+        SpringApplication.run(SearchServiceApplication.class, args);
+    }
+}
