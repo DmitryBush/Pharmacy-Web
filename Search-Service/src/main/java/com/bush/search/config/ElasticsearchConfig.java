6b09baf843628d62dc05bdaf29e5a4b5a48a0c5a
Dmitry Bush <bushuev0458@gmail.com>
2026-02-17 22:40:12 +0300

    Created es ad Kafka configs

---
+++ Search-Service/src/main/java/com/bush/search/config/ElasticsearchConfig.java
@@ -0,0 +1,28 @@
+package com.bush.search.config;
+
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.bush.search.domain.document")
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+    @Value("${spring.elsaticsearch.url}")
+    private String url;
+    @Value("${spring.elsaticsearch.username}")
+    private String username;
+    @Value("${spring.elsaticsearch.password}")
+    private String password;
+
+    @Override
+    @NonNull
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(url)
+                .withBasicAuth(username, password)
+                .build();
+    }
+}
