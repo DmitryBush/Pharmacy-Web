64026b08a30896c83755b847de7bfce52fabf544
Dmitry Bush <bushuev0458@gmail.com>
2026-02-19 22:47:40 +0300

    feat(mapper):Implemented create mappers

---
+++ Search-Service/src/main/java/com/bush/search/service/country/mapper/CountryCreateMapper.java
@@ -0,0 +1,13 @@
+package com.bush.search.service.country.mapper;
+
+import com.bush.search.domain.document.manufacturer.Country;
+import com.bush.search.domain.index.manufacturer.CountryPayload;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface CountryCreateMapper {
+    @Mapping(target = "id", expression = "java(String.valueOf(countryPayload.id()))")
+    Country mapToCountry(CountryPayload countryPayload);
+}
---
+++ Search-Service/src/main/java/com/bush/search/service/product/mapper/ProductCreateMapper.java
@@ -0,0 +1,17 @@
+package com.bush.search.service.product.mapper;
+
+import com.bush.search.domain.document.product.Product;
+import com.bush.search.domain.index.product.ProductPayload;
+import com.bush.search.service.manufacturer.mapper.ManufacturerCreateMapper;
+import com.bush.search.service.supplier.mapper.SupplierCreateMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, imports = String.class,
+        uses = {ManufacturerCreateMapper.class, SupplierCreateMapper.class})
+public interface ProductCreateMapper {
+    @Mapping(target = "id", expression = "java(String.valueOf(productPayload.id()))")
+    @Mapping(target = "productId", source = "id")
+    Product mapToProduct(ProductPayload productPayload);
+}
