120141b8892a2a9768e49298997ddb7471f75809
Dmitry Bush <bushuev0458@gmail.com>
2026-02-19 22:48:33 +0300

    feat(product):Implemented ES CRUD operations

---
+++ Search-Service/src/main/java/com/bush/search/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.bush.search.repository;
+
+import com.bush.search.domain.document.product.Product;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface ProductRepository extends ElasticsearchRepository<Product, String> {
+}
--- Search-Service/src/main/java/com/bush/search/service/product/ProductService.java
+++ Search-Service/src/main/java/com/bush/search/service/product/ProductService.java
@@ -1,18 +1,36 @@
 package com.bush.search.service.product;
 
 import com.bush.search.domain.index.product.ProductPayload;
+import com.bush.search.repository.ProductRepository;
+import com.bush.search.service.product.mapper.ProductCreateMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    private final ProductCreateMapper productCreateMapper;
 
     public void createProduct(ProductPayload productPayload) {
-
+        Optional.ofNullable(productPayload)
+                .map(productCreateMapper::mapToProduct)
+                .map(productRepository::save);
     }
 
     public void updateProduct(String id, ProductPayload productPayload) {
-
+        Optional.ofNullable(id)
+                .flatMap(productRepository::findById)
+                .map(_ -> productCreateMapper.mapToProduct(productPayload))
+                .map(productRepository::save);
     }
 
     public void deleteProduct(String id) {
-
+        Optional.ofNullable(id)
+                .flatMap(productRepository::findById)
+                .ifPresent(productRepository::delete);
     }
 }
