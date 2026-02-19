8ec47555936c8c1502ad6481fef35be4facaac05
Dmitry Bush <bushuev0458@gmail.com>
2026-02-19 21:47:10 +0300

    fix(handler):added group id

--- Search-Service/src/main/java/com/bush/search/event/kafka/index/handler/product/ProductCdcEventHandler.java
+++ Search-Service/src/main/java/com/bush/search/event/kafka/index/handler/product/ProductCdcEventHandler.java
@@ -4,16 +4,18 @@ import com.bush.search.domain.index.CdcEvent;
 import com.bush.search.event.kafka.index.handler.CdcEventHandler;
 import com.bush.search.event.kafka.index.handler.strategy.index.ResolveIndexStrategyRegistry;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@KafkaListener(topics = "search-pharmacies.public.outbox_service_table")
+@KafkaListener(topics = "search-pharmacies.public.outbox_service_table", groupId = "product-processing-group")
 @RequiredArgsConstructor
 public class ProductCdcEventHandler implements CdcEventHandler<String> {
     private final ResolveIndexStrategyRegistry indexStrategyRegistry;
 
     @Override
+    @KafkaHandler
     public void handle(CdcEvent<String> event) {
         indexStrategyRegistry.resolveIndexStrategy(event.objectName(), event.crudOperationConstantsType(), event.payload());
     }
