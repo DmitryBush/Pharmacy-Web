07a497786a866e4919bf369b8b2ae764c81b3582
Dmitry Bush <bushuev0458@gmail.com>
2026-03-01 01:11:25 +0300

    feat(cache):Added static resource caching

---
+++ src/main/java/com/bush/pharmacy_web_app/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.bush.pharmacy_web_app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**", "/css/**")
+                .addResourceLocations("classpath:/static/js/", "classpath:/static/css/")
+                .setCacheControl(CacheControl.maxAge(182, TimeUnit.DAYS).cachePublic());
+    }
+}
