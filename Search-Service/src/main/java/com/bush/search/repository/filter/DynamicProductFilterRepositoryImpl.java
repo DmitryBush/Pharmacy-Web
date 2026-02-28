f985285fcd8904b5e6c0cbaf7a45ca3af694deee
Dmitry Bush <bushuev0458@gmail.com>
2026-02-28 18:45:51 +0300

    feat(aggregation):Added global aggregations

--- Search-Service/src/main/java/com/bush/search/repository/filter/DynamicProductFilterRepositoryImpl.java
+++ Search-Service/src/main/java/com/bush/search/repository/filter/DynamicProductFilterRepositoryImpl.java
@@ -3,9 +3,11 @@ package com.bush.search.repository.filter;
 import co.elastic.clients.elasticsearch._types.aggregations.Aggregate;
 import co.elastic.clients.elasticsearch._types.aggregations.Aggregation;
 import co.elastic.clients.elasticsearch._types.aggregations.Buckets;
+import co.elastic.clients.elasticsearch._types.aggregations.GlobalAggregate;
 import co.elastic.clients.elasticsearch._types.aggregations.MultiBucketBase;
 import co.elastic.clients.elasticsearch._types.aggregations.NestedAggregate;
 import co.elastic.clients.elasticsearch._types.aggregations.StringTermsAggregate;
+import co.elastic.clients.elasticsearch._types.aggregations.StringTermsBucket;
 import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import co.elastic.clients.util.ObjectBuilder;
@@ -14,6 +16,7 @@ import com.bush.search.domain.dto.ProductFilter;
 import com.bush.search.repository.FilterResultTuple;
 import jakarta.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -26,8 +29,8 @@ import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -38,6 +41,9 @@ import java.util.stream.Stream;
 @Repository
 @RequiredArgsConstructor
 public class DynamicProductFilterRepositoryImpl implements DynamicProductFilterRepository {
+    @Value("${spring.elsaticsearch.aggregation.bucket-size}")
+    private Integer aggregationBucketSize;
+
     private final ElasticsearchOperations elasticsearchClient;
 
     @Override
@@ -59,17 +65,34 @@ public class DynamicProductFilterRepositoryImpl implements DynamicProductFilterR
     private NativeQuery buildNativeQuery(BoolQuery boolQuery) {
         return NativeQuery.builder()
                 .withQuery(boolQuery._toQuery())
-                .withAggregation("manufacturers", createNestedAggregation("manufacturer",
-                        "manufacturer.name.keyword"))
-                .withAggregation("countries", createNestedAggregation("manufacturer.country",
-                        "manufacturer.country.countryName.keyword"))
-                .withAggregation("activeIngredients", Aggregation.of(a -> a.terms(t ->
-                        t.field("activeIngredient").size(100))))
+                .withAggregation("globalManufacturers", createGlobalAggregation("manufacturers",
+                        "manufacturer", "manufacturer.name.keyword"))
+                .withAggregation("globalCountries", createGlobalAggregation("countries",
+                        "manufacturer.country", "manufacturer.country.countryName.keyword"))
+                .withAggregation("globalActiveIngredients",
+                        createGlobalAggregation("activeIngredients", "activeIngredient"))
                 .build();
     }
 
+    private Aggregation createGlobalAggregation(String aggregationName, String fieldName) {
+        Map<String, Aggregation> aggregations = new HashMap<>();
+        aggregations.put(aggregationName, createAggregation(fieldName));
+        return Aggregation.of(a -> a.global(g -> g).aggregations(aggregations));
+    }
+
+    private Aggregation createGlobalAggregation(String aggregationName, String nestedObjectName, String fieldName) {
+        Map<String, Aggregation> aggregationMap = new HashMap<>();
+        aggregationMap.put(aggregationName, createNestedAggregation(nestedObjectName, fieldName));
+        return Aggregation.of(a -> a.global(g -> g).aggregations(aggregationMap));
+    }
+
+    private Aggregation createAggregation(String fieldName) {
+        return Aggregation.of(a -> a.terms(t -> t.field(fieldName).size(aggregationBucketSize)));
+    }
+
     private Aggregation createNestedAggregation(String nestedObjectName, String fieldName) {
-        Aggregation aggregation = Aggregation.of(a -> a.terms(t -> t.field(fieldName).size(100)));
+        Aggregation aggregation = Aggregation.of(a ->
+                a.terms(t -> t.field(fieldName).size(aggregationBucketSize)));
         return Aggregation.of(a -> a.nested(n -> n.path(nestedObjectName)).aggregations(fieldName, aggregation));
     }
 
@@ -150,32 +173,50 @@ public class DynamicProductFilterRepositoryImpl implements DynamicProductFilterR
         if (aggregationContainer.getClass().isAssignableFrom(ElasticsearchAggregations.class)) {
             ElasticsearchAggregations esAggs = (ElasticsearchAggregations) aggregationContainer;
 
-            Map<String, Long> manufacturers = getBucketNestedAggregation(esAggs.get("manufacturers"));
-            Map<String, Long> countries = getBucketNestedAggregation(esAggs.get("countries"));
-            Map<String, Long> activeIngredients = getBucketAggregation(esAggs.get("activeIngredients"));
+            Map<String, Long> manufacturers = getGlobalNestedAggregation(esAggs.get("globalManufacturers"));
+            Map<String, Long> countries = getGlobalNestedAggregation(esAggs.get("globalCountries"));
+            Map<String, Long> activeIngredients = getGlobalAggregation(esAggs.get("globalActiveIngredients"));
             return new ProductAggregation(manufacturers, countries, activeIngredients);
         }
         throw new IllegalArgumentException("Unknown aggregation container class");
     }
 
-    private Map<String, Long> getBucketAggregation(ElasticsearchAggregation aggregation) {
-        return Optional.ofNullable(aggregation).stream()
-                .map(ElasticsearchAggregation::aggregation)
-                .map(org.springframework.data.elasticsearch.client.elc.Aggregation::getAggregate)
-                .map(Aggregate::sterms).map(StringTermsAggregate::buckets)
-                .map(Buckets::array).flatMap(Collection::stream)
+    private Map<String, Long> getGlobalAggregation(ElasticsearchAggregation aggregation) {
+        return getGlobalAggregateStream(aggregation)
+                .flatMap(this::getStringTermsBucketStream)
                 .collect(Collectors.toMap(b -> b.key().stringValue(), MultiBucketBase::docCount));
     }
 
-    private Map<String, Long> getBucketNestedAggregation(ElasticsearchAggregation aggregation) {
+    private Map<String, Long> getGlobalNestedAggregation(ElasticsearchAggregation aggregation) {
+        return getGlobalAggregateStream(aggregation)
+                .flatMap(this::getNestedAggregateStream)
+                .flatMap(this::getStringTermsBucketStream)
+                .collect(Collectors.toMap(b -> b.key().stringValue(), MultiBucketBase::docCount));
+    }
+
+    private Stream<Aggregate> getGlobalAggregateStream(ElasticsearchAggregation aggregation) {
         return Optional.ofNullable(aggregation).stream()
                 .map(ElasticsearchAggregation::aggregation)
                 .map(org.springframework.data.elasticsearch.client.elc.Aggregation::getAggregate)
-                .map(Aggregate::nested).map(NestedAggregate::aggregations).map(Map::values)
-                .flatMap(Collection::stream).map(Aggregate::sterms)
+                .map(Aggregate::global)
+                .map(GlobalAggregate::aggregations)
+                .map(Map::values)
+                .flatMap(Collection::stream);
+    }
+
+    private Stream<Aggregate> getNestedAggregateStream(Aggregate aggregate) {
+        return Optional.of(aggregate).stream()
+                .map(Aggregate::nested)
+                .map(NestedAggregate::aggregations)
+                .map(Map::values)
+                .flatMap(Collection::stream);
+    }
+
+    private Stream<StringTermsBucket> getStringTermsBucketStream(Aggregate aggregateStream) {
+        return Optional.of(aggregateStream).stream()
+                .map(Aggregate::sterms)
                 .map(StringTermsAggregate::buckets)
                 .map(Buckets::array)
-                .flatMap(Collection::stream)
-                .collect(Collectors.toMap(b -> b.key().stringValue(), MultiBucketBase::docCount));
+                .flatMap(Collection::stream);
     }
 }
