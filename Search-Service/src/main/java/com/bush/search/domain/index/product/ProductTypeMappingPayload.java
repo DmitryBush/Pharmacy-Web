7c78c598e661bb1c9de9c2167c1fb9a2bffe846f
Dmitry Bush <bushuev0458@gmail.com>
2026-03-01 18:50:46 +0300

    feat(type):Added slug support to search service

--- Search-Service/src/main/java/com/bush/search/domain/index/product/ProductTypeMappingPayload.java
+++ Search-Service/src/main/java/com/bush/search/domain/index/product/ProductTypeMappingPayload.java
@@ -2,5 +2,6 @@ package com.bush.search.domain.index.product;
 
 public record ProductTypeMappingPayload(Integer id,
                                         String type,
+                                        String slug,
                                         Boolean isMain) {
 }
--- Search-Service/src/main/java/com/bush/search/domain/index/product/ProductTypePayload.java
+++ Search-Service/src/main/java/com/bush/search/domain/index/product/ProductTypePayload.java
@@ -1,6 +1,7 @@
 package com.bush.search.domain.index.product;
 
 public record ProductTypePayload(Integer id,
-                                 String type,
+                                 String name,
+                                 String slug,
                                  ProductTypeMappingPayload parent) {
 }
