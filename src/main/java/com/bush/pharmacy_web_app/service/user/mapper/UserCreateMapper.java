1b6e35883dbded4295b7f7fbe7900df81c2781aa
Dmitry Bush <bushuev0458@gmail.com>
2026-03-10 14:08:44 +0300

    feat(security):Added argon password hashing

--- src/main/java/com/bush/pharmacy_web_app/service/user/mapper/UserCreateMapper.java
+++ src/main/java/com/bush/pharmacy_web_app/service/user/mapper/UserCreateMapper.java
@@ -3,12 +3,16 @@ package com.bush.pharmacy_web_app.service.user.mapper;
 import com.bush.pharmacy_web_app.model.dto.user.CustomerCreateDto;
 import com.bush.pharmacy_web_app.model.entity.user.User;
 import com.bush.pharmacy_web_app.shared.mapper.DtoMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 
 @Component
+@RequiredArgsConstructor
 public class UserCreateMapper implements DtoMapper<CustomerCreateDto, User> {
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User map(CustomerCreateDto obj) {
@@ -20,12 +24,12 @@ public class UserCreateMapper implements DtoMapper<CustomerCreateDto, User> {
         return copyObj(fromObj, toObj);
     }
 
-    private static User copyObj(CustomerCreateDto fromObj, User toObj) {
+    private User copyObj(CustomerCreateDto fromObj, User toObj) {
         toObj.setName(fromObj.name());
         toObj.setSurname(fromObj.surname());
         toObj.setLastName(fromObj.lastName());
         toObj.setMobilePhone(fromObj.mobilePhone());
-        toObj.setPassword(fromObj.password());
+        toObj.setPassword(passwordEncoder.encode(fromObj.password()));
         toObj.setOrders(Collections.emptyList());
         return toObj;
     }
