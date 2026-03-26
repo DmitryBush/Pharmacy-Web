bf13ed6bae826d96a2a958b02b8e3211ea7b5a16
Dmitry Bush <bushuev0458@gmail.com>
2026-03-26 14:46:50 +0300

    feat(branch):Implemented base info editing form

--- src/main/java/com/bush/pharmacy_web_app/controllers/http/admin/BranchManagementController.java
+++ src/main/java/com/bush/pharmacy_web_app/controllers/http/admin/BranchManagementController.java
@@ -51,4 +51,17 @@ public class BranchManagementController {
         model.addAttribute("currentUri", request.getRequestURI());
         return "admin/branch/branch-info";
     }
+
+    @GetMapping("/{id}/general")
+    public String getBranchGeneralInfoEditingPage(Model model, HttpServletRequest request,
+                                                  @AuthenticationPrincipal UserDetails userDetails) {
+        var authorities = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        model.addAttribute("authorities", authorities);
+        model.addAttribute("currentUri", request.getRequestURI());
+        return "admin/branch/branch-base-info-editing";
+    }
 }
---
+++ src/main/resources/static/js/admin/branch-base-info-editing.js
@@ -0,0 +1,53 @@
+import RestClient from "../RestClient.js";
+import Notification from "../notification/notification.js";
+
+document.addEventListener("DOMContentLoaded", async e => {
+    const restClient = new RestClient();
+    const notification = new Notification();
+
+    const generalInfoForm = document.getElementById('branch-general-info-editing-form');
+
+    const saveBtn = document.getElementById('save-btn');
+    const cancelBtn = document.getElementById('cancel-btn');
+
+    saveBtn.addEventListener('click', async e => {
+        e.preventDefault();
+        try {
+            await saveForm();
+            notification.showNotification('Управление филиалами',
+                'Обновление основной информации филиала произошло успешно');
+            setTimeout(() => window.location.replace('/admin/branch/1'), 1500);
+        } catch (e) {
+            console.error(e);
+            notification.showNotification('Управление филиалами',
+                `Произошла ошибка при обновлении основной информации филиала. Код ${e}`);
+        }
+    });
+    cancelBtn.addEventListener('click', e => window.location.replace('/admin/branch/1'));
+
+    try {
+        await initializePage();
+    } catch (e) {
+        console.error(e);
+        notification.showNotification('Управление филиалами',
+            `Произошла ошибка при инициализации страницы. Код ${e}`);
+    }
+
+    async function initializePage() {
+        const branchInfo = await (await restClient.fetchData(`/api/v1/admin/branches/1`, 'GET')).json();
+        fillForm(branchInfo);
+    }
+
+    function fillForm(branchInfo) {
+        generalInfoForm.querySelector('input[id=name]').value = branchInfo.name;
+        generalInfoForm.querySelector('input[id=warehouseLimitations]').value = branchInfo.warehouseLimitations;
+        generalInfoForm.querySelector('input[id=contactPhone]').value = branchInfo.branchPhone;
+        generalInfoForm.querySelector('input[type=checkbox][name=isActive]').checked = branchInfo.isActive;
+    }
+
+    async function saveForm() {
+        const formData = new FormData(generalInfoForm);
+        await restClient.fetchData(`/api/v1/admin/branches/1`, 'PATCH',
+            {'Content-type': 'application/json'}, JSON.stringify(formData));
+    }
+});
