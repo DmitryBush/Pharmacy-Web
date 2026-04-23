76efec14ac9c0e4d815ef1b2ede5f964da936857
Dmitry Bush <bushuev0458@gmail.com>
2026-04-23 20:37:01 +0300

    refactor:Changed method name to lowcase

--- src/main/java/com/bush/pharmacy_web_app/controllers/http/order/CustomerOrderController.java
+++ src/main/java/com/bush/pharmacy_web_app/controllers/http/order/CustomerOrderController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CustomerOrderController {
 
     @GetMapping("/place")
-    public String PlaceOrder(Model model) {
+    public String placeOrder(Model model) {
         return "order/placing-order";
     }
 }
