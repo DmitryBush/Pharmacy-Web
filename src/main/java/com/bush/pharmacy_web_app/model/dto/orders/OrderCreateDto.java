fe8cb618e69a964dfaffeb95eafc90a7dc16e0e2
Dmitry Bush <bushuev0458@gmail.com>
2026-04-23 14:37:14 +0300

    fix:Improve dto validation

--- src/main/java/com/bush/pharmacy_web_app/model/dto/orders/OrderCreateDto.java
+++ src/main/java/com/bush/pharmacy_web_app/model/dto/orders/OrderCreateDto.java
@@ -1,9 +1,10 @@
 package com.bush.pharmacy_web_app.model.dto.orders;
 
 import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
-public record OrderCreateDto(@NotNull List<OrderItemCreateDto> orderItems,
+public record OrderCreateDto(@Validated @NotNull List<OrderItemCreateDto> orderItems,
                              @NotNull Long branchId) {
 }
--- src/main/java/com/bush/pharmacy_web_app/model/dto/orders/OrderItemCreateDto.java
+++ src/main/java/com/bush/pharmacy_web_app/model/dto/orders/OrderItemCreateDto.java
@@ -1,8 +1,11 @@
 package com.bush.pharmacy_web_app.model.dto.orders;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public record OrderItemCreateDto(Long productId,
-                                 Integer quantity,
-                                 BigDecimal price) {
+public record OrderItemCreateDto(@NotNull Long productId,
+                                 @NotNull @Min(0) Integer quantity,
+                                 @NotNull BigDecimal price) {
 }
