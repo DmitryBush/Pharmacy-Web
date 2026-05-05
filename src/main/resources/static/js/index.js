a089d246756de73646cbfb9911f333f5ce1f243e
Dmitry Bush <bushuev0458@gmail.com>
2026-05-05 18:01:02 +0300

    fix:Fixed product grid displaying

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -8,11 +8,13 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     let cartItemsSet = new Set();
 
+    const dailyProductsSection = document.getElementById("daily-product-section");
     const dailyProductsContainer = document.getElementById('product-container');
-    const dailyProductsLoader = new Loader(dailyProductsContainer);
+    const dailyProductsLoader = new Loader(dailyProductsSection);
 
+    const newsSection = document.getElementById('news-section');
     const newsContainer = document.getElementById('news-container');
-    const newsLoader = new Loader(newsContainer);
+    const newsLoader = new Loader(newsSection);
 
     try {
         await initialize();
