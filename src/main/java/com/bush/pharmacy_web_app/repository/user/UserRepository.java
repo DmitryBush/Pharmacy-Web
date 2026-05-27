31d7914c4ffe35bc0638650699a8ab8756828ae1
Dmitry Bush <bushuev0458@gmail.com>
2026-05-27 22:49:20 +0300

    performance:Optimized user query

--- src/main/java/com/bush/pharmacy_web_app/repository/user/UserRepository.java
+++ src/main/java/com/bush/pharmacy_web_app/repository/user/UserRepository.java
@@ -4,12 +4,14 @@ import com.bush.pharmacy_web_app.model.entity.user.User;
 import com.bush.pharmacy_web_app.model.entity.user.role.RoleType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
@@ -21,4 +23,9 @@ public interface UserRepository extends JpaRepository<User, String> {
     Page<User> findAllByMobilePhoneAndRole(@Param("mobilePhone") String mobilePhone,
                                            @Param("type") List<RoleType> type,
                                            Pageable pageable);
+
+    @Query("select u from User u " +
+            "join fetch u.role")
+    @Override
+    Optional<User> findById(String s);
 }
