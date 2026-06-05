ff4626e7ac771a5c1eb0708860238a5b61d44e88
Dmitry Bush <bushuev0458@gmail.com>
2026-06-05 22:52:19 +0300

    fix:Fixed product constraint and cart interface

--- src/main/resources/static/js/cart/cart-utils.js
+++ src/main/resources/static/js/cart/cart-utils.js
@@ -5,7 +5,11 @@ export async function getCartProductsSet() {
     const set = new Set();
     try {
         const cart = await (await restClient.fetchData(`/api/v1/carts/me`, 'GET')).json();
-        cart.cartItems.forEach(item => set.add(item.medicine.id));
+        if (cart._embedded === undefined) {
+            return set;
+        } else {
+            cart._embedded.CartItemReadDto.forEach(item => set.add(item.medicine.id));
+        }
     } catch (error) {
         if (error.message !== '401') {
             throw error;
